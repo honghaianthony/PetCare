3cab98eda9674cf9e8dc0c2cdc3232c31b46bd92
Anthony Hai <namsaoquan@gmail.com>
2022-10-01 20:36:42 +0700

    Add src layout

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { NextUIProvider } from "@nextui-org/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <NextUIProvider>
+                <App />
+            </NextUIProvider>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+function About() {
+    return (
+        <div>
+            <Icon icon="akar-icons:facebook-fill" />
+        </div>
+    );
+}
+
+export default About;
