7468c8ebf15abcb1db4a8d41c6529b1d11df21ae
Anthony Hai <namsaoquan@gmail.com>
2022-10-11 21:59:12 +0700

    feat(auth): add login/register page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as Home } from "./Home";
 export { default as About } from "./About";
 export { default as Service } from "./Service";
+export { default as Login } from "./Auth/login";
+export { default as Register } from "./Auth/register";
