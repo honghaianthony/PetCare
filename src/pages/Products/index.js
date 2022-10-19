2c91a72dbcffea7b7aeaa07d6e30fa6f774e6913
hishirokaiyashi <hishirokaiyashi@gmail.com>
2022-10-19 20:59:44 +0700

    products chua xong

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -2,18 +2,11 @@ import React from 'react'
 import MainLayout from "../../layouts/MainLayout";
 import ProductsBanner from '../../components/ProductsBanner';
 import ProductsContent from '../../components/ProductsContent';
-import ProductsMenu from '../../components/ProductsMenu';
 const Products = () => {
   return (
     <MainLayout>
         <ProductsBanner/>
-        <ProductsMenu/>
-        {/* <Route exact path="/doan">
-          <ProductsContent/>
-        </Route>
-        <Route exact path="/quanao">
-          <ProductsContent/>
-        </Route> */}
+        <ProductsContent/>
     </MainLayout>
   )
 }
