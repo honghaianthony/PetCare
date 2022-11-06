04e148e4129b4672c6b33a106948c9e842e2d125
DuyenTM <tmdmduyen@gmail.com>
2022-11-06 20:19:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Router from "./routes";
 
 function App() {
-    return (
-        <div className="App">
-            <Router />
-        </div>
-    );
+  const [toogle, setToogle] = useState(true);
+  return (
+    <div className={`App ${toogle ? "first-layout" : "second-layout"}`}>
+      <button
+        style={{ position: "fixed", zIndex: 9999 }}
+        onClick={() => setToogle(!toogle)}
+      >
+        Change layout
+      </button>
+      <Router />
+    </div>
+  );
 }
 
 export default App;
