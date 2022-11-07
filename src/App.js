6a33a7e0c42ff7420fc1f33361eee87ae85adaca
Anthony Hai <namsaoquan@gmail.com>
2022-11-07 21:16:27 +0700

    Done merge 07/11

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import "./App.css";
 import Router from "./routes";
 
 function App() {
-  const [toogle, setToogle] = useState(true);
-  return (
-    <div className={`App ${toogle ? "first-layout" : "second-layout"}`}>
-      <button
-        style={{ position: "fixed", zIndex: 9999 }}
-        onClick={() => setToogle(!toogle)}
-      >
-        Change layout
-      </button>
-      <Router />
-    </div>
-  );
+    const [toogle, setToogle] = useState(true);
+    return (
+        <div className={`App ${toogle ? "first-layout" : "second-layout"}`}>
+            <button
+                style={{ position: "fixed", zIndex: 9999 }}
+                onClick={() => setToogle(!toogle)}
+            >
+                Change layout
+            </button>
+            <Router />
+        </div>
+    );
 }
 
 export default App;
