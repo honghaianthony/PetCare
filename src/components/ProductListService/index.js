2505043d18b53f090ec443bf57283f0cc958e934
hishirokaiyashi <hishirokaiyashi@gmail.com>
2022-11-11 01:30:35 +0700

    done service layout 2

--- src/components/ProductListService/index.js
+++ src/components/ProductListService/index.js
@@ -1,101 +1,280 @@
 import React from "react";
-import "./ProductListService.css"
+import "./ProductListService.css";
 function ProductListService() {
-    return (
-        <section className="Product__list__container">
-            <div className="Product__list__container__detail">
-                <h3 className="text__header__Product__list">
-                    MỘT SỐ DỊCH VỤ TIÊU BIỂU CỦA CHÚNG TÔI:
-                </h3>
-                <div className="underline__service__Product__list"></div>
-                <div className="text__content__Product__list">
-                    <h4>
-                        Các dịch vụ cơ bản:
-                    </h4>
-                    <div className="Product__list__text">
-                        <h5 className="Product__list__number">
-                            1. Gói cắt và vệ sinh lông chó mèo:
-
-                        </h5>
-                        <p>
-                            Cùng với sự nóng lên toàn cầu nên mức nhiệt ở Tp.Hồ Chính Minh luôn ở mức cao. Do đó, việc vệ sinh và cắt tỉa lông để cho thú cưng luôn trong trạng thái sạch sẽ, thoáng mát, thoải mái là điều thật sự cần thiết để giúp cho thú cưng tránh được các loại bệnh tật về da. Gói cắt và vệ sinh lông chó mèo luôn là lựa chọn phổ biến được ưa chuộng trong tất cả các dịch vụ mà PetCare cung cấp. Quy trình cắt và vệ sinh lông thú cưng như sau:
-                        </p>
-                        <ul className="Product__list__">
-                            <li>Kiểm tra da và lông chó mèo nhằm xác định tình trạng da và lông của chúng.</li>
-                            <li>Tư vấn cho chủ nuôi các kiểu cắt lông và vệ sinh phù hợp với tình trạng thú cưng cho khách lựa chọn.</li>
-                            <li>Cạo và vệ sinh lông ở các vùng chân, bụng và hậu môn cho chó mèo.</li>
-                            <li>Sấy lông nhằm loại bỏ lông thừa dính trên thân các em thú cưng.</li>
-                            <li>Chải lông cho bé thú cưng.</li>
-                            <li>Xịt nước hoa cao cấp chuyên dụng để lưu lại hương thơm và dưỡng lông cho chó mèo.</li>
-                        </ul>
-                        <div className="Product__list_service__img__container">
-                            <img
-                                src="/assets/images/Service/spa-thu-cung-dich-vu-thuong-hang-dogily-pet-shop.jpg"
-                                alt="spa-thu-cung-dich-vu-thuong-hang-dogily-pet-shop"
-                                className='Product__list_service__img'
-                            />
-                        </div>
-                    </div>
-                    <div className="Product__list__text">
-                        <h5 className="Product__list__number">
-                            2. Gói tắm thú cưng:
-                        </h5>
-                        <p>
-                            Gói Spa tắm cho thú cưng bao gồm 2 loại hình chính là tắm cho chó và tắm cho mèo. Và việc tắm sấy chó mèo tại hệ thống Spa thú cưng của Dogily PetCare đều phải trải qua những quy trình nghiêm ngặt sau:                        </p>
-                        <ul className="Product__list__">
-                            <li>Đầu tiên, chúng tôi sẽ kiểm tra da và lông bé chó, mèo nhằm xác định nên sử dụng loại dầu tắm nào cho thích hợp.
-                            </li>
-                            <li>Tiếp đến là bóp dịch hậu môn nhằm khử mùi hôi hoàn toàn trên thân chó, mèo.
-                            </li>
-                            <li>Bước thứ 3 là xả nước sạch lên thú cưng để loại bỏ chất bẩn trước khi thoa dầu tắm. Sau đó, chuyên viên chăm sóc sẽ nhẹ nhàng massage và loại bỏ bụi bẩn trên lông chó, mèo với bộ dụng cụ chuyên nghiệp.
-                            </li>
-                            <li>Bước thứ 4, chúng tôi sẽ xả sạch sữa tắm và sử dụng dầu xả để dưỡng ẩm và tạo sự mềm mượt cho bộ da, lông của chó, mèo.
-                            </li>
-                            <li>Tiếp theo nữa là dùng nước xả sạch dầu xả rồi sấy khô và chải lông để loại bỏ lông chết còn dính trên lông chó, mèo.
-                            </li>
-                            <li>Sau đó, thoa một lớp lotion mỏng để tạo độ bóng mượt tự nhiên cho bộ lông thú cưng.
-                            </li>
-                            <li>
-                                Cuối cùng, xịt nước hoa để tạo hương thơm lưu lại lâu dài trên cơ thể bé chó, mèo.
-                            </li>
-                        </ul>
-                        <div className="Product__list_service__img__container">
-                            <img
-                                src="/assets/images/Service/dich-vu-spa-tam-thu-cung-chuyen-nghiep.jpg"
-                                alt="dich-vu-spa-tam-thu-cung-chuyen-nghiep"
-                                className='Product__list_service__img'
-                            />
-                        </div>
-                    </div>
-                    <div className="Product__list__text">
-                        <h5 className="Product__list__number">
-                            3. Gói cắt tỉa và tạo kiểu lông chó mèo.
-                        </h5>
-                        <p>
-                            Hiện nay, không chỉ là cắt lông, vệ sinh lông chó mèo thông thường mà người nuôi còn muốn cắt tỉa lông để tạo ra những kiểu lông xinh xắn, độc lạ chó bé cưng của họ. Chính vì thế, đội ngũ chuyên viên của Spa thú cưng PetCare cũng luôn luôn cập nhật những xu hướng làm đẹp cho thú cưng mới nhất trên thế giới để đáp ứng nhu cầu đó của các chủ nuôi. Quy trình cắt tỉa lông chó mèo để tạo kiểu sẽ phải trải qua các bước sau:                        </p>
-                        <ul className="Product__list__">
-                            <li>Kiểm tra da và lông chó mèo nhằm xác định tình trạng da và lông của chúng.
-                            </li>
-                            <li>Dựa vào mật độ, chất lông và độ dài lông của bé chó, mèo để tư vấn các mẫu, kiểu cắt lông phù hợp.
-                            </li>
-                            <li>Sau khi chủ nuôi chọn được kiểu lông muốn cắt tỉa cho thú cưng, chuyên viên của chúng tôi sẽ dùng kéo và tông đơ cắt lông chuyên nghiệp để cắt tỉa lông chó.
-                            </li>
-                            <li>Sấy lông nhằm loại bỏ lông thừa dính trên thân bé thú cưng.</li>
-                            <li>Chải lông cho bé thú cưng.</li>
-                            <li>Xịt nước hoa cao cấp chuyên dụng để lưu lại hương thơm và dưỡng lông cho chó mèo.</li>
-                        </ul>
-                        <div className="Product__list_service__img__container">
-                            <img
-                                src="/assets/images/Service/spa-cat-tia-long-meo-chuyen-nghiep.jpg"
-                                alt="spa-cat-tia-long-meo-chuyen-nghiep"
-                                className='Product__list_service__img'
-                            />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <section className="Product__list__container">
+        <div className="Product__list__container__detail">
+          <h3 className="text__header__Product__list">
+            MỘT SỐ DỊCH VỤ TIÊU BIỂU CỦA CHÚNG TÔI:
+          </h3>
+          <div className="underline__service__Product__list"></div>
+          <div className="text__content__Product__list">
+            <h4>Các dịch vụ cơ bản:</h4>
+            <div className="Product__list__text">
+              <h5 className="Product__list__number">
+                1. Gói cắt và vệ sinh lông chó mèo:
+              </h5>
+              <p>
+                Cùng với sự nóng lên toàn cầu nên mức nhiệt ở Tp.Hồ Chính Minh
+                luôn ở mức cao. Do đó, việc vệ sinh và cắt tỉa lông để cho thú
+                cưng luôn trong trạng thái sạch sẽ, thoáng mát, thoải mái là
+                điều thật sự cần thiết để giúp cho thú cưng tránh được các loại
+                bệnh tật về da. Gói cắt và vệ sinh lông chó mèo luôn là lựa chọn
+                phổ biến được ưa chuộng trong tất cả các dịch vụ mà PetCare cung
+                cấp. Quy trình cắt và vệ sinh lông thú cưng như sau:
+              </p>
+              <ul className="Product__list__">
+                <li>
+                  Kiểm tra da và lông chó mèo nhằm xác định tình trạng da và
+                  lông của chúng.
+                </li>
+                <li>
+                  Tư vấn cho chủ nuôi các kiểu cắt lông và vệ sinh phù hợp với
+                  tình trạng thú cưng cho khách lựa chọn.
+                </li>
+                <li>
+                  Cạo và vệ sinh lông ở các vùng chân, bụng và hậu môn cho chó
+                  mèo.
+                </li>
+                <li>
+                  Sấy lông nhằm loại bỏ lông thừa dính trên thân các em thú
+                  cưng.
+                </li>
+                <li>Chải lông cho bé thú cưng.</li>
+                <li>
+                  Xịt nước hoa cao cấp chuyên dụng để lưu lại hương thơm và
+                  dưỡng lông cho chó mèo.
+                </li>
+              </ul>
+              <div className="Product__list_service__img__container">
+                <img
+                  src="/assets/images/Service/spa-thu-cung-dich-vu-thuong-hang-dogily-pet-shop.jpg"
+                  alt="spa-thu-cung-dich-vu-thuong-hang-dogily-pet-shop"
+                  className="Product__list_service__img"
+                />
+              </div>
             </div>
-        </section>
-    )
+            <div className="Product__list__text">
+              <h5 className="Product__list__number">2. Gói tắm thú cưng:</h5>
+              <p>
+                Gói Spa tắm cho thú cưng bao gồm 2 loại hình chính là tắm cho
+                chó và tắm cho mèo. Và việc tắm sấy chó mèo tại hệ thống Spa thú
+                cưng của Dogily PetCare đều phải trải qua những quy trình nghiêm
+                ngặt sau:{" "}
+              </p>
+              <ul className="Product__list__">
+                <li>
+                  Đầu tiên, chúng tôi sẽ kiểm tra da và lông bé chó, mèo nhằm
+                  xác định nên sử dụng loại dầu tắm nào cho thích hợp.
+                </li>
+                <li>
+                  Tiếp đến là bóp dịch hậu môn nhằm khử mùi hôi hoàn toàn trên
+                  thân chó, mèo.
+                </li>
+                <li>
+                  Bước thứ 3 là xả nước sạch lên thú cưng để loại bỏ chất bẩn
+                  trước khi thoa dầu tắm. Sau đó, chuyên viên chăm sóc sẽ nhẹ
+                  nhàng massage và loại bỏ bụi bẩn trên lông chó, mèo với bộ
+                  dụng cụ chuyên nghiệp.
+                </li>
+                <li>
+                  Bước thứ 4, chúng tôi sẽ xả sạch sữa tắm và sử dụng dầu xả để
+                  dưỡng ẩm và tạo sự mềm mượt cho bộ da, lông của chó, mèo.
+                </li>
+                <li>
+                  Tiếp theo nữa là dùng nước xả sạch dầu xả rồi sấy khô và chải
+                  lông để loại bỏ lông chết còn dính trên lông chó, mèo.
+                </li>
+                <li>
+                  Sau đó, thoa một lớp lotion mỏng để tạo độ bóng mượt tự nhiên
+                  cho bộ lông thú cưng.
+                </li>
+                <li>
+                  Cuối cùng, xịt nước hoa để tạo hương thơm lưu lại lâu dài trên
+                  cơ thể bé chó, mèo.
+                </li>
+              </ul>
+              <div className="Product__list_service__img__container">
+                <img
+                  src="/assets/images/Service/dich-vu-spa-tam-thu-cung-chuyen-nghiep.jpg"
+                  alt="dich-vu-spa-tam-thu-cung-chuyen-nghiep"
+                  className="Product__list_service__img"
+                />
+              </div>
+            </div>
+            <div className="Product__list__text">
+              <h5 className="Product__list__number">
+                3. Gói cắt tỉa và tạo kiểu lông chó mèo.
+              </h5>
+              <p>
+                Hiện nay, không chỉ là cắt lông, vệ sinh lông chó mèo thông
+                thường mà người nuôi còn muốn cắt tỉa lông để tạo ra những kiểu
+                lông xinh xắn, độc lạ chó bé cưng của họ. Chính vì thế, đội ngũ
+                chuyên viên của Spa thú cưng PetCare cũng luôn luôn cập nhật
+                những xu hướng làm đẹp cho thú cưng mới nhất trên thế giới để
+                đáp ứng nhu cầu đó của các chủ nuôi. Quy trình cắt tỉa lông chó
+                mèo để tạo kiểu sẽ phải trải qua các bước sau:{" "}
+              </p>
+              <ul className="Product__list__">
+                <li>
+                  Kiểm tra da và lông chó mèo nhằm xác định tình trạng da và
+                  lông của chúng.
+                </li>
+                <li>
+                  Dựa vào mật độ, chất lông và độ dài lông của bé chó, mèo để tư
+                  vấn các mẫu, kiểu cắt lông phù hợp.
+                </li>
+                <li>
+                  Sau khi chủ nuôi chọn được kiểu lông muốn cắt tỉa cho thú
+                  cưng, chuyên viên của chúng tôi sẽ dùng kéo và tông đơ cắt
+                  lông chuyên nghiệp để cắt tỉa lông chó.
+                </li>
+                <li>
+                  Sấy lông nhằm loại bỏ lông thừa dính trên thân bé thú cưng.
+                </li>
+                <li>Chải lông cho bé thú cưng.</li>
+                <li>
+                  Xịt nước hoa cao cấp chuyên dụng để lưu lại hương thơm và
+                  dưỡng lông cho chó mèo.
+                </li>
+              </ul>
+              <div className="Product__list_service__img__container">
+                <img
+                  src="/assets/images/Service/spa-cat-tia-long-meo-chuyen-nghiep.jpg"
+                  alt="spa-cat-tia-long-meo-chuyen-nghiep"
+                  className="Product__list_service__img"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="Product__list__container_second">
+        <div className="Product__list__container__detail_second">
+          <h3>Một Số Dịch Vụ Tiêu Biểu</h3>
+          <div className="Product__list__item_container2">
+            <div className="Product__list__item2">
+              <img
+                src="/assets/images/Service/dog-shower.jpg"
+                alt="dog-shower"
+                className="Product__list__item2_img"
+              />
+              <div className="Product__list__item2_container_bottom">
+                <h3 className="Product__list__item2_text">Gói tắm thú cưng</h3>
+                <p className="Product__list__item2_text_content">
+                  -Kiểm tra da và lông bé chó, mèo nhằm xác định nên sử dụng
+                  loại dầu tắm nào cho thích hợp. Bóp dịch hậu môn nhằm khử mùi
+                  hôi hoàn toàn trên thân chó, mèo.
+                  <br></br>
+                  -Xả nước sạch lên thú cưng để loại bỏ chất bẩn trước khi thoa
+                  dầu tắm, chuyên viên chăm sóc sẽ nhẹ nhàng massage và loại bỏ
+                  bụi bẩn trên lông chó, mèo với bộ dụng cụ chuyên nghiệp.
+                  <br></br>
+                  -Xả sạch sữa tắm và sử dụng dầu xả để dưỡng ẩm và tạo sự mềm
+                  mượt cho bộ da, lông của chó, mèo.
+                  <br></br>
+                  -thoa một lớp lotion mỏng để tạo độ bóng mượt tự nhiên cho bộ
+                  lông thú cưng
+                </p>
+              </div>
+            </div>
+
+            <div className="Product__list__item2">
+              <img
+                src="/assets/images/Service/5bfe577e8aab8de9b0bc456011c6b031.png"
+                alt="dog-cutting"
+                className="Product__list__item2_img"
+              />
+              <div className="Product__list__item2_container_bottom">
+                <h3 className="Product__list__item2_text">
+                  Gói cắt và vệ sinh lông chó mèo
+                </h3>
+                <p className="Product__list__item2_text_content">
+                  -Kiểm tra da và lông chó mèo nhằm xác định tình trạng da và
+                  lông của chúng.
+                  <br></br>
+                  -Tư vấn cho chủ nuôi các kiểu cắt lông và vệ sinh phù hợp với
+                  tình trạng thú cưng cho khách lựa chọn.
+                  <br></br>
+                  -Cạo và vệ sinh lông ở các vùng chân, bụng và hậu môn cho chó
+                  mèo.
+                  <br></br>
+                  -Sấy lông nhằm loại bỏ lông thừa dính trên thân các em thú
+                  cưng.
+                  <br />
+                  -Chải lông cho bé thú cưng.
+                  <br />
+                  -Xịt nước hoa cao cấp chuyên dụng để lưu lại hương thơm và
+                  dưỡng lông cho chó mèo.
+                </p>
+              </div>
+            </div>
+
+            <div className="Product__list__item2">
+              <img
+                src="/assets/images/Service/dich-vu-cat-tia-long-thu-cung-uy-tin-nhat-tai-hai-phong-anh-minh-dam-481658.jpg"
+                alt="dog-shower"
+                className="Product__list__item2_img"
+              />
+              <div className="Product__list__item2_container_bottom">
+                <h3 className="Product__list__item2_text">
+                  Gói cắt tỉa tạo kiểu lông chó mèo
+                </h3>
+                <p className="Product__list__item2_text_content">
+                  -Kiểm tra da và lông chó mèo nhằm xác định tình trạng da và
+                  lông của chúng.
+                  <br></br>
+                  -Tư vấn các mẫu, kiểu cắt lông phù hợp.
+                  <br></br>
+                  -Dùng kéo và tông đơ cắt lông chuyên nghiệp để cắt tỉa lông
+                  chó.
+                  <br></br>
+                  -Sấy lông nhằm loại bỏ lông thừa dính trên thân bé thú cưng.
+                  <br />
+                  -Chải lông cho bé thú cưng.
+                  <br />
+                  -Xịt nước hoa cao cấp chuyên dụng để lưu lại hương thơm và
+                  dưỡng lông cho chó mèo.
+                </p>
+              </div>
+            </div>
+
+            <div className="Product__list__item2">
+              <img
+                src="/assets/images/Service/2dv.jpg"
+                alt="dog-shower"
+                className="Product__list__item2_img"
+              />
+              <div className="Product__list__item2_container_bottom">
+                <h3 className="Product__list__item2_text">
+                  Gói huấn luyện chó cảnh{" "}
+                </h3>
+                <p className="Product__list__item2_text_content">
+                  -Thời lượng học lý tưởng là từ 1- 2 lần/ ngày (vào sáng và
+                  chiều), mỗi lần khoảng 15 – 30 phút.
+                  <br></br>
+                  -Gói huấn luyện bao gồm:
+                  <br />
+                  +Huấn luyện chó nằm
+                  <br />
+                  +Huấn luyện chó ngồi.
+                  <br />
+                  +Huấn luyện chó không ăn bậy
+                  <br />
+                  +Huấn luyện chó nhặt đồ
+                  <br />
+                  +Huấn luyện chó không cắn đồ
+                  <br />
+                  +Huấn luyện chó giả chết
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default ProductListService;
--- src/components/ServiceChoice/index.js
+++ src/components/ServiceChoice/index.js
@@ -1,38 +1,120 @@
-import React from 'react'
-import "./ServiceChoice.css"
-import { Icon } from '@iconify/react';
-import "@fontsource/nunito"
-const ServiceChoice = () => {
+import React from "react";
+import "./ServiceChoice.css";
+import { Icon } from "@iconify/react";
+import "@fontsource/nunito";
+const ServiceChoice = ({handleClick}) => {
+  // console.log(handleClick)
   return (
-    <section className='service__choice__container'>
+    <>
+      <section className="service__choice__container">
         <div className="service__choice__container__detail">
           <h3 className="text__header__choice">
-               3 ĐIỀU KHIẾN CHO PETCARE TRỞ THÀNH LỰA CHỌN HÀNG ĐẦU CỦA BẠN:
+            3 ĐIỀU KHIẾN CHO PETCARE TRỞ THÀNH LỰA CHỌN HÀNG ĐẦU CỦA BẠN:
           </h3>
           <div className="underline__service__choice"></div>
           <div className="service_choice_content_container">
             <div className="service_choice_content">
-                 <Icon icon="ant-design:check-circle-twotone" color="white" className="icon__service"/>
-                 <p className='text__choice__service'> 
-                     LUÔN ĐẶT YÊU CẦU KHÁCH HÀNG Ở VỊ TRÍ CAO NHẤT
-                 </p>
+              <Icon
+                icon="ant-design:check-circle-twotone"
+                color="white"
+                className="icon__service"
+              />
+              <p className="text__choice__service">
+                LUÔN ĐẶT YÊU CẦU KHÁCH HÀNG Ở VỊ TRÍ CAO NHẤT
+              </p>
             </div>
             <div className="service_choice_content">
-                   <Icon icon="icon-park-outline:good-two" color="white" className="icon__service"/>
-                 <p className='text__choice__service'>
-                 CHẤT LƯỢNG HÀNG ĐẦU TRONG THỊ TRƯỜNG
-                 </p>
+              <Icon
+                icon="icon-park-outline:good-two"
+                color="white"
+                className="icon__service"
+              />
+              <p className="text__choice__service">
+                CHẤT LƯỢNG HÀNG ĐẦU TRONG THỊ TRƯỜNG
+              </p>
             </div>
             <div className="service_choice_content">
-                   <Icon icon="dashicons:money-alt" color="white" className="icon__service"/>
-                 <p className='text__choice__service'>
-                 GIÁ THÀNH DỊCH VỤ Ở MỨC TỐT NHẤT
-                 </p>
+              <Icon
+                icon="dashicons:money-alt"
+                color="white"
+                className="icon__service"
+              />
+              <p className="text__choice__service">
+                GIÁ THÀNH DỊCH VỤ Ở MỨC TỐT NHẤT
+              </p>
             </div>
           </div>
         </div>
-    </section>
-  )
-}
+      </section>
+      <section className="service__choice__container_second">
+        <div className="service__choice__container__detail_second">
+          <h3>DỊCH VỤ DÀNH CHO THÚ CƯNG</h3>
+          <div className="service__choice__content_2">
+            <div className="service__choice__content_item_2">
+              <img
+                src="/assets/images/Service/iconHotel@2x.png"
+                alt="Huấn luyện"
+                className="service__choice__content_item_img_2"
+              />
+              <h3>Huấn luyện</h3>
+              <button
+              className="service__choice__content_item_button_2"
+              onClick={handleClick}
+              >
+                Đặt lịch ngay
+              </button>
+            </div>
+            
+            <div className="service__choice__content_item_2">
+              <img
+                src="/assets/images/Service/iconSpa@2x.png"
+                alt="Làm Đẹp"
+                className="service__choice__content_item_img_2"
+              />
+              <h3>Làm Đẹp</h3>
+              <button
+              className="service__choice__content_item_button_2"
+              onClick={handleClick}
+              >
+                Đặt lịch ngay
+              </button>
+            </div>
 
-export default ServiceChoice
+            <div className="service__choice__content_item_2">
+              <img
+                src="/assets/images/Service/iconVet@2x.png"
+                alt="Phòng Khám"
+                className="service__choice__content_item_img_2"
+              />
+              <h3>Phòng Khám</h3>
+              <button
+              className="service__choice__content_item_button_2"
+              onClick={handleClick}
+              >
+                Đặt lịch ngay
+              </button>
+            </div>
+
+            <div className="service__choice__content_item_2">
+              <img
+                src="/assets/images/Service/iconShop@2x.png"
+                alt="Mua Sắm"
+                className="service__choice__content_item_img_2"
+              />
+              <h3>Mua Sắm</h3>
+              <button
+              className="service__choice__content_item_button_2"
+              onClick={handleClick}
+              >
+                Đặt lịch ngay
+              </button>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ServiceChoice;
