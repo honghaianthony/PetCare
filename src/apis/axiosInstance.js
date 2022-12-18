37eaecc6a1da4e9a1b106fb6cd2fb46b6ba2166e
Anthony Hai <namsaoquan@gmail.com>
2022-12-18 13:12:34 +0700

    Done merge 18/12

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
-    // baseURL: "https://petcare-be.vercel.app/api/v1",
+    // baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://petcare-be.vercel.app/api/v1",
 });
 
 axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${Cookies.get(
