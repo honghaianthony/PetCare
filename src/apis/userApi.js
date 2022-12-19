74b366d90f437e4af9ab84fb3f8248d2e7ce7c46
DuyenTM <tmdmduyen@gmail.com>
2022-12-19 22:43:32 +0700

    done history

--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -1,23 +1,34 @@
 import axiosInstance from "./axiosInstance";
 
 const getAllUsers = async () => {
-    return await axiosInstance.get("/user/get-all-users");
+  return await axiosInstance.get("/user/get-all-users");
 };
 
 const getUserById = async (id) => {
-    return await axiosInstance.get(`/user/get-user-by-id?userId=${id}`);
+  return await axiosInstance.get(`/user/get-user-by-id?userId=${id}`);
 };
 
 const updateUserRole = async (data) => {
-    return await axiosInstance.put(`user/update-users-role`, data);
+  return await axiosInstance.put(`user/update-users-role`, data);
 };
 
 const updateUser = async (data, id) => {
-    return await axiosInstance.put(`user/update-user?id=${id}`, data);
+  return await axiosInstance.put(`user/update-user?id=${id}`, data);
 };
 
 const deleteUser = async (id) => {
-    return await axiosInstance.delete(`/user/delete-user?userId=${id}`);
+  return await axiosInstance.delete(`/user/delete-user?userId=${id}`);
 };
 
-export { getUserById, getAllUsers, updateUserRole, updateUser, deleteUser };
+const getUser = async () => {
+  return await axiosInstance.get("/user/get-info");
+};
+
+export {
+  getUserById,
+  getAllUsers,
+  updateUserRole,
+  updateUser,
+  deleteUser,
+  getUser,
+};
