06daf69a3fc05bfae9fd647767cd16f7dc8d3ce4
Anthony Hai <namsaoquan@gmail.com>
2022-12-19 23:18:28 +0700

    fix: routes in image

--- src/components/BlogCommon/index.js
+++ src/components/BlogCommon/index.js
@@ -1,24 +1,30 @@
 import React from "react";
 import "./BlogCommon.css";
 const BlogCommon = () => {
-  return (
-    <section className="BlogCommon_container">
-      <div className="BlogCommon_container_detail">
-        <div className="BlogCommon_container_detail_item">
-          <div className="BlogCommon_container_left">
-            <div className="BlogCommon_container_img_left_container">
-              <img src="/assets/images/Blog/bannerDog.png" alt="bannerDog" />
+    return (
+        <section className="BlogCommon_container">
+            <div className="BlogCommon_container_detail">
+                <div className="BlogCommon_container_detail_item">
+                    <div className="BlogCommon_container_left">
+                        <div className="BlogCommon_container_img_left_container">
+                            <img
+                                src="/assets/images/blog/bannerDog.png"
+                                alt="bannerDog"
+                            />
+                        </div>
+                    </div>
+                    <div className="BlogCommon_container_right">
+                        <div className="BlogCommon_container_img_right_container">
+                            <img
+                                src="/assets/images/blog/bannerCat.png"
+                                alt="bannerCat"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="BlogCommon_container_right">
-            <div className="BlogCommon_container_img_right_container">
-              <img src="/assets/images/Blog/bannerCat.png" alt="bannerCat" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default BlogCommon;
